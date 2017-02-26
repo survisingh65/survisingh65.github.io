4bf0bf3fe7d4034b22a7ba16eb236c4bff2b9b58
survisingh65 <singh.surv@husky.neu.edu>
2017-02-26 11:56:29 -0500

    User Service Update

--- project_EventManagement/server/services/user.service.server.js
+++ project_EventManagement/server/services/user.service.server.js
@@ -3,6 +3,12 @@ module.exports = function(app, userModel) {
     app.post("/api/em/userlogin", verifyLogin);
     app.post("/api/em/userpreference", savePreference);
 
+    var users = [];
+        users = [
+             {	"_id":123, "firstName":"Survi",            "lastName":"Singh",
+                 "username":"survi",  "password":"survi",   "roles": ["admin"]		}
+         ]
+
     function register(req, res) {
         var user = req.body;
         userModel.createUser(user)
@@ -21,18 +27,27 @@ module.exports = function(app, userModel) {
 
     function verifyLogin(req, res) {
         var user = req.body;
-        userModel.findUserByCredential(user)
-            .then(
-                // login user if promise resolved
-                function ( doc ) {
-                    //req.session.currentUser = doc;
-                    res.json(doc);
-                },
-                // send error if promise rejected
-                function ( err ) {
-                    res.status(400).send(err);
-                }
-            );
+         var foundUser = null;
+        for (var i = 0; i < users.length; i++) {
+            var u = users[i];
+            if (u.username == user.username && u.password == user.password) {
+                foundUser = u;
+             }
+         }
+        return res.foundUser;
+
+        //userModel.findUserByCredential(user)
+        //    .then(
+        //        // login user if promise resolved
+        //        function ( doc ) {
+        //            //req.session.currentUser = doc;
+        //            res.json(doc);
+        //        },
+        //        // send error if promise rejected
+        //        function ( err ) {
+        //            res.status(400).send(err);
+        //        }
+        //    );
     }
 
 
