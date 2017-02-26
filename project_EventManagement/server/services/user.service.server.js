d65d030a35c940bfb92200028e7f5f34b2ff3e8d
survisingh65 <singh.surv@husky.neu.edu>
2017-02-26 11:59:23 -0500

    Roll Backed User Service Changes

--- project_EventManagement/server/services/user.service.server.js
+++ project_EventManagement/server/services/user.service.server.js
@@ -3,12 +3,6 @@ module.exports = function(app, userModel) {
     app.post("/api/em/userlogin", verifyLogin);
     app.post("/api/em/userpreference", savePreference);
 
-    var users = [];
-        users = [
-             {	"_id":123, "firstName":"Survi",            "lastName":"Singh",
-                 "username":"singh.surv@husky.neu.edu",  "password":"survi",   "roles": ["admin"]		}
-         ]
-
     function register(req, res) {
         var user = req.body;
         userModel.createUser(user)
@@ -27,27 +21,18 @@ module.exports = function(app, userModel) {
 
     function verifyLogin(req, res) {
         var user = req.body;
-         var foundUser = null;
-        for (var i = 0; i < users.length; i++) {
-            var u = users[i];
-            if (u.username == user.username && u.password == user.password) {
-                foundUser = u;
-             }
-         }
-        return res.foundUser;
-
-        //userModel.findUserByCredential(user)
-        //    .then(
-        //        // login user if promise resolved
-        //        function ( doc ) {
-        //            //req.session.currentUser = doc;
-        //            res.json(doc);
-        //        },
-        //        // send error if promise rejected
-        //        function ( err ) {
-        //            res.status(400).send(err);
-        //        }
-        //    );
+        userModel.findUserByCredential(user)
+            .then(
+                // login user if promise resolved
+                function ( doc ) {
+                    //req.session.currentUser = doc;
+                    res.json(doc);
+                },
+                // send error if promise rejected
+                function ( err ) {
+                    res.status(400).send(err);
+                }
+            );
     }
 
 
